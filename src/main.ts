c1c20616938949e69142540e16bf4ab81912e5b8
aoaochan <josejbeck99@yahoo.com>
2025-06-09 21:09:27 +0900

    asdf

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
-console.log("Start!");
+import { Application } from "pixi.js";
+import { ZstPackage } from "./zstd.js";
+
+const app = new Application();
+
+async function setup() {
+  await app.init({ background: '#000000', resizeTo: window });
+  app.canvas.style.position = "absolute";
+  document.body.appendChild(app.canvas);
+}
+
+async function preload() {
+  const zstCdnUrl = "";
+  const pkg = await ZstPackage.open(zstCdnUrl);
+}
+
+(async () => {
+  await setup();
+  await preload();
+})();
